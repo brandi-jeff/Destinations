26677b389baef4b6576029a4fe43c3b0509edc73
brandi.jeff <brandi.jeff2@gmail.com>
2023-02-24 14:22:42 -0500

    finally figured out the fetch request for images

--- scripts/main.js
+++ scripts/main.js
@@ -46,16 +46,12 @@ function storeUserInput() {
 }
 
 const defaultPhoto = "./defaultPic.PNG";
-let eventFlag = false;
+let eventFlag = false; //flag to show card is being created not, edited
 
 function addToCard(img, dest, loc, desc) {
   const groupDiv = document.createElement("div");
   groupDiv.setAttribute("class", "card-group");
   flexDiv.appendChild(groupDiv);
-
-  //   const colDiv = document.createElement("div");
-  //   colDiv.setAttribute("class", "col");
-  //   rowDiv.appendChild(colDiv);
 
   const cardDiv = document.createElement("div");
   cardDiv.setAttribute("id", "cardDiv");
@@ -64,15 +60,15 @@ function addToCard(img, dest, loc, desc) {
 
   const imgTag = document.createElement("img");
   imgTag.setAttribute("class", "card-img-top");
-  imgTag.setAttribute("class", "flex-grow-1");
+  // imgTag.setAttribute("class", "flex-grow-1");
   imgTag.setAttribute("alt", dest);
   imgTag.style.width = "15rem";
   imgTag.style.height = "15rem";
   cardDiv.appendChild(imgTag); //append inside div with id=dest-card
 
   if (img === "") {
-    // getImg(cardDiv, img);
-    imgTag.setAttribute("src", defaultPhoto);
+    getImg(cardDiv);
+    // imgTag.setAttribute("src", defaultPhoto);
   } else {
     imgTag.setAttribute("src", img);
   }
@@ -124,7 +120,6 @@ function addToCard(img, dest, loc, desc) {
 
 // edit button to change card
 function editCard(cardDiv) {
-  eventFlag = true;
 
   cardDiv.newDest = prompt("Enter new destination city:");
   cardDiv.newLoc = prompt("Enter new destination state/country:");
@@ -132,11 +127,15 @@ function editCard(cardDiv) {
   cardDiv.newDesc = prompt("Update description:");
 
   if (cardDiv.newImg !== null && cardDiv.newImg.length > 0) {
-    // getImg(cardDiv, cardDiv.newDest);
+    //find parent element since all the cards have the same id and classes by default since they are dynamically created
     const img = cardDiv.currentTarget.parentElement.parentElement.parentElement;
     img.querySelector("img").setAttribute("src", cardDiv.newImg);
+  } else {
+    eventFlag = true; //now true because the card exists, and needs to be edited bc the edit event occurred
+    getImg(cardDiv, cardDiv.newDest);
   }
 
+  // find parent element so edits correct card
   const oldParentDest = cardDiv.currentTarget.parentElement.parentElement;
   const oldDest = oldParentDest.querySelector("#dest-name");
   if (cardDiv.newDest !== null && cardDiv.newDest.length === 0) {
@@ -158,13 +157,12 @@ function editCard(cardDiv) {
   if (cardDiv.newDesc !== null && cardDiv.newDesc.length === 0) {
     oldDesc.innerText;
   } else if (cardDiv.newDesc.length > 0) {
-    if (oldDesc.innerText === undefined) {
+    if (oldDesc.innerText === undefined) { //if user didnt eneter a description when the card was made, need to create that element
       const destDesc = document.createElement("p");
       destDesc.setAttribute("id", "dest-desc");
       cardBody.appendChild(destDesc);
       destDesc.innerText = cardDiv.newDesc;
     }
-
     oldDesc.innerText = cardDiv.newDesc;
   }
 }
@@ -177,28 +175,26 @@ function removeCard(cardDiv) {
 }
 
 // use API for photos
-// function getImg(cardDiv) { //figure out why wont work when update photo, or why it updates wrong photo
-
-//     if (eventFlag) {
-//         const dest = document.getElementById("dest-name").value;
-
-//         const url = `https://api.unsplash.com/search/photos?query=${dest}&client_id=VrE3yua9qa8PzQJ8uOdw1FPsC5l7IELgHg2fp5yCBqI`;
-//         fetch(url).then(response => response.json())
-//         .then(photos=>  {
-//             const stockImg = photos.results[5].urls.thumb;
-//             cardDiv.querySelector(".card-img-top")
-//             .setAttribute("src", stockImg);
-//         });
-
-//     } else {
-//        const dest = document.getElementById("dest-input").value;
-
-//         const url = `https://api.unsplash.com/search/photos?query=${dest}&client_id=VrE3yua9qa8PzQJ8uOdw1FPsC5l7IELgHg2fp5yCBqI`;
-//         fetch(url).then(response => response.json())
-//         .then(photos=>  {
-//             const stockImg = photos.results[5].urls.thumb;
-//             cardDiv.querySelector(".card-img-top")
-//             .setAttribute("src", stockImg);
-//         });
-//     }
-// }
+function getImg(cardDiv, dest) { //figure out why wont work when update photo, or why it updates wrong photo
+  
+  if (eventFlag) {
+      const url = `https://api.unsplash.com/search/photos?query=${dest}&client_id=VrE3yua9qa8PzQJ8uOdw1FPsC5l7IELgHg2fp5yCBqI`;
+      fetch(url).then(response => response.json())
+      .then(photos=>  {
+          editImg = cardDiv.srcElement;
+          const newImg = editImg.parentElement.parentElement.parentElement;
+          const stockImg = photos.results[5].urls.thumb;
+          newImg.querySelector(".card-img-top")
+          .setAttribute("src", stockImg);
+      });
+  } else {
+     dest = document.getElementById("dest-input").value;
+      const url = `https://api.unsplash.com/search/photos?query=${dest}&client_id=VrE3yua9qa8PzQJ8uOdw1FPsC5l7IELgHg2fp5yCBqI`;
+      fetch(url).then(response => response.json())
+      .then(photos=>  {
+          const stockImg = photos.results[5].urls.thumb;
+          cardDiv.querySelector(".card-img-top")
+          .setAttribute("src", stockImg);
+      });
+  }
+}
